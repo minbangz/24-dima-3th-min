66d0e8a1ae3b4adf25a41472acca210d017f9b20
minbangz <163627024+minbangz@users.noreply.github.com>
2024-05-25 07:15:17 +0000

    console.log

---
+++ script.js
@@ -0,0 +1,28 @@
+//console.log('HELLO');
+//Commentary
+//alert('hello');
+var a = 10;
+var b;
+b = 100;
+console.log (a + b);
+console.log (a / b);
+console.log (7 % 2);
+console.log (5 % 3);
+console.log (3 % 2);
+console.log (7 % 4);
+console.log (6 % 2);
+console.log (7 > 3);
+console.log (7 < 3);
+console.log (7 >= 7);
+console.log (3 <= 3);
+console.log (7 == 7);
+console.log (7 != 7);
+console.log (7 === '7');
+console.log (7+parseInt('2',10));
+console.log ((3).toString());
+var name = 'mainbagz';
+console.log (`안녕하세요,${name}`);
+console.log (`안녕하세요,+ name`);
+var p = 'cat dog alligator';
+console.log (p.indexOf('dog'));
+console.log (p.indexOf('cat'));
